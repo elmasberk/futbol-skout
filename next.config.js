7f431d7224af3e0a4d9fb61484c248b10f5618c9
Berk Elmas <berkelmas@Berks-MacBook-Air.local>
2026-03-10 22:01:49 +0300

    next config duzeltildi

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Capacitor için statik export gerekli
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true, // Static export'ta next/image optimize edemez
-  },
-}
+const nextConfig = {}
 module.exports = nextConfig
